60bfbaedf64931b8f0e37b6702531f21d73cfc78
Alexius Lim <Alexius.Lim@rpsgroup.com>
2022-04-22 10:43:21 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ColumnModel, PageService, SelectionService, SelectionSettingsModel } from '@syncfusion/ej2-angular-grids';
+import { orderData } from './order-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [PageService, SelectionService]
 })
 export class AppComponent {
-  title = 'grid-table';
+  pageSettings = {
+    pageSize: 5
+  };
+
+  data = orderData;
+  selectionSettings: SelectionSettingsModel = {
+    type: 'Single',
+    mode: 'Row',
+    persistSelection: true
+  };
+  columns: ColumnModel[] = [
+    { field: 'OrderID', headerText: 'Order ID', isPrimaryKey: true },
+    { field: 'CustomerName', headerText: 'Customer Name' },
+    { field: 'OrderDate', headerTemplate: 'Order Date' }
+  ];
+
+  changePageSize() {
+    this.pageSettings = {
+      pageSize: this.pageSettings.pageSize === 5 ? 3 : 5
+    }
+  }
 }
